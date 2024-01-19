1658f07b490c384aa38d39f8148b86aa7aa9db7f
csJoax <joax@qq.com>
2024-01-19 17:05:35 +0800

    added[demo/text_classification/]: train.py and predict.py

---
+++ demo/text_classification/predict.py
@@ -0,0 +1,33 @@
+import os
+
+# os.environ["TL_BACKEND"] = "torch"
+# os.environ['TL_BACKEND'] = 'paddle'
+os.environ["TL_BACKEND"] = "tensorflow"
+
+from tlxnlp.tasks.text_classification import TextClassification
+from tlxnlp.models.transform import BertTransform
+from tlxnlp.models import Bert
+import tensorlayerx as tlx
+
+if __name__ == "__main__":
+    backbone = Bert()
+    model = TextClassification(backbone)
+    model.load_weights("./demo/text_classification/model.npz")
+    model.set_eval()
+
+    transform = BertTransform(
+        vocab_file="./demo/text_classification/vocab.txt", max_length=128
+    )
+
+    text = "it 's a charming and often affecting journey ."
+    x, y = transform(text, None)
+
+    inputs = tlx.convert_to_tensor([x["inputs"]])
+    token_type_ids = tlx.convert_to_tensor([x["token_type_ids"]])
+    attention_mask = tlx.convert_to_tensor([x["attention_mask"]])
+
+    _logits = model(
+        inputs=inputs, token_type_ids=token_type_ids, attention_mask=attention_mask
+    )
+    label = tlx.argmax(_logits, axis=-1)
+    print(label)
